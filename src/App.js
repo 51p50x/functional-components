95e5546f3cf9929b19d8461fcc17f4d8c00887fb
51p50x <51plog50@gmail.com>
2022-03-15 14:46:53 -0600

    sc3: similar to the previous branch sc2 but this time is a class component doing a counter instead of usestate hook

--- src/App.js
+++ src/App.js
@@ -1,9 +1,24 @@
-import {useState} from 'react'
+import {Component, useState} from 'react'
 
 //hooks rules, don't call a hook inside a loop or a flow control structure
 //hooks rules, just call a hook in 2 parts: in react component and custom hooks
 //hooks rules, when creating a custom it always start with 'hook use'
-const App = () => {
+class App extends Component {
+    state = {contador: 0}
+    incrementar = ()=>{
+        this.setState({contador: this.state.contador + 1})
+    }
+    render(){
+        return(
+            <div>
+                Contador: {this.state.contador}
+                <br/>
+                <button onClick={this.incrementar}>Incrementar</button>
+            </div>
+        )
+    }
+}
+/*const App = () => {
     const [contador, setContador] = useState(0)
     return(
         <div>
@@ -12,6 +27,6 @@ const App = () => {
             <button onClick={()=>setContador(contador + 1)}>Incrementar</button>
         </div>
     )
-}
+}*/
 
 export default App
