7986ba8dc62926336e913c4d7628dd63bf08e6ea
51p50x <51plog50@gmail.com>
2022-03-15 14:21:20 -0600

    fc1: created 2 pure functional components and 1 impure function just to see how behaves

--- src/App.js
+++ src/App.js
@@ -1,3 +1,18 @@
+const impura = () => new Date().toLocaleString()
+console.log(impura())
 
+const MiComponente = ({miProp}) =>{
+  return(
+    <div>
+      Nombre: {miProp}
+    </div>
+  )
+}
 
-export default App;
+const App = () =>{
+  return(
+    <MiComponente miProp = {'hello world'} />
+  )
+}
+
+export default App
