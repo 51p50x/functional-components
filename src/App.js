d6e70780761fdd8bf6a3a75f372a36bbc8dc0d23
51p50x <51plog50@gmail.com>
2022-03-15 14:40:38 -0600

    sc2: created a basic use of useState hook

--- src/App.js
+++ src/App.js
@@ -1,18 +1,17 @@
-const impura = () => new Date().toLocaleString()
-console.log(impura())
+import {useState} from 'react'
 
-const MiComponente = ({miProp}) =>{
-  return(
-    <div>
-      Nombre: {miProp}
-    </div>
-  )
-}
-
-const App = () =>{
-  return(
-    <MiComponente miProp = {'hello world'} />
-  )
+//hooks rules, don't call a hook inside a loop or a flow control structure
+//hooks rules, just call a hook in 2 parts: in react component and custom hooks
+//hooks rules, when creating a custom it always start with 'hook use'
+const App = () => {
+    const [contador, setContador] = useState(0)
+    return(
+        <div>
+            Contador: {contador}
+            <br/>
+            <button onClick={()=>setContador(contador + 1)}>Incrementar</button>
+        </div>
+    )
 }
 
 export default App
