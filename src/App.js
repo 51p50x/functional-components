4db16fe5cdd97db3c2c9b0e5f3ef49569a63bbf5
51p50x <51plog50@gmail.com>
2022-03-15 19:09:54 -0600

    fc4: created simple example with useEffect hook, it will update the html title everytime that you press the increase button

--- src/App.js
+++ src/App.js
@@ -1,32 +1,25 @@
-import {Component, useState} from 'react'
+import {useState, useEffect} from 'react'
 
-//hooks rules, don't call a hook inside a loop or a flow control structure
-//hooks rules, just call a hook in 2 parts: in react component and custom hooks
-//hooks rules, when creating a custom it always start with 'hook use'
-class App extends Component {
-    state = {contador: 0}
-    incrementar = ()=>{
-        this.setState({contador: this.state.contador + 1})
+const useContador = (inicial) => {
+    const [contador, setContador] = useState(inicial)
+    const incrementar = () => {
+        setContador(contador + 1)
     }
-    render(){
-        return(
-            <div>
-                Contador: {this.state.contador}
-                <br/>
-                <button onClick={this.incrementar}>Incrementar</button>
-            </div>
-        )
-    }
+    return [contador, incrementar]
 }
-/*const App = () => {
-    const [contador, setContador] = useState(0)
+
+const App = () => {
+    const [contador, incrementar] = useContador(0)
+    useEffect(()=>{
+        document.title = contador
+    },[contador])
     return(
         <div>
             Contador: {contador}
             <br/>
-            <button onClick={()=>setContador(contador + 1)}>Incrementar</button>
+            <button onClick={incrementar}>Incrementar</button>
         </div>
     )
-}*/
+}
 
 export default App
